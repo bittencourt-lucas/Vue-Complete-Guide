eef41ab1d745a6a45485afba3447c8059e6dd85e
Lucas Cafieiro A. Bittencourt <lbittencourt@Arch0N.localdomain>
2022-01-24 07:53:19 -0300

    Understanding events on Vue

--- vue-2/first-app/hello.js
+++ vue-2/first-app/hello.js
@@ -3,13 +3,23 @@ new Vue({
   data: {
     title: 'Hello World!',
     link: 'https://www.google.com',
-    finishedLink: '<a href="https://www.google.com">Google</a>"'
+    finishedLink: '<a href="https://www.google.com">Google</a>"',
+    counter: 0,
+    x: 0,
+    y: 0,
   },
   methods: {
     sayHello: function() {
       this.title = 'Hello!';
       return this.title;
-    }
-  }
+    },
+    increase: function() {
+      this.counter++;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+  },
 });
   
